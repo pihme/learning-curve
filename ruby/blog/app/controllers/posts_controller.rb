9898d16ea14679af565f96c9f1b53d385bed7d47
pihme <pihme@users.noreply.github.com>
2016-04-16 16:21:28 +0200

    finished tutorial

--- ruby/blog/app/controllers/posts_controller.rb
+++ ruby/blog/app/controllers/posts_controller.rb
@@ -1,10 +1,8 @@
 class PostsController < ApplicationController
 
   def new
-
+    @post = Post.new
   end
-
-
 
   def index
     @posts = Post.all
@@ -18,12 +16,37 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find_by_id(params[:id])
+  end
+
   def create
     @post = Post.new(post_params)
 
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
 
   def post_params
     params.require(:post).permit(:title, :body)
